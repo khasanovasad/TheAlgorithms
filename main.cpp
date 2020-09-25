e8a3d2cc38cb90f60644d5a76b16389cb91334fc
khasanovasad <asadbek1khasanov@gmail.com>
2020-09-25 22:06:43 +0500

    test out

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <string>
 #include <iostream>
+#include <string>
 
 #include "SLinkedList.h"
 #include "DLinkedList.h"
@@ -12,6 +12,7 @@ void PrintList(DLinkedList<int> *d_list) {
 }
 
 int main() {
+    /*
     do {
         auto *d_list = new DLinkedList<int>();
         for (int i = 0; i < 10; ++i) {
@@ -26,6 +27,23 @@ int main() {
         PrintList(d_list);
         d_list->RemoveBack();
         PrintList(d_list);
+        delete d_list;
+    } while (true);
+    */
+    /*
+    do {
+        auto *s_list = new SLinkedList<std::string>({ zero, one, two, three, four });
+        for (int i = 0; i < s_list->Size(); ++i)
+            std::cout << s_list->ElementAt(i) << " ";
+        std::cout << std::endl;
+        delete s_list;
+    } while (true);
+    */
+    do {
+        auto *d_list = new DLinkedList<std::string>({"zero", "one", "two", "three", "four"});
+        for (int i = 0; i < d_list->Count(); ++i)
+            std::cout << d_list->ElementAt(i) << " ";
+        std::cout << std::endl;
         delete d_list;
     } while (true);
     return 0;
