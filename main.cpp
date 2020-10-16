467233016e0825364faa798d458f6803c9018984
khasanovasad <asadbek1khasanov@gmail.com>
2020-10-16 17:50:31 +0500

    testing Queue.h

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@
 #include "SLinkedList.h"
 #include "DLinkedList.h"
 #include "Stack.h"
+#include "Queue.h"
 
 using namespace DSCpp;
 
 int main(int args, char *argv[]) {
+    /*
     do {
         Stack<std::string> myStack; 
 
@@ -25,5 +27,24 @@ int main(int args, char *argv[]) {
             std::cout << myStack.Pop() << "\n";
         }
     } while (false);
+    */
+    do {
+        Queue<int> myQueue;
+
+        std::cout << "Pushing...\n";
+        for (int i = 0; i < 10; ++i) {
+            std::cout << i << "\n";
+            myQueue.Push(i);
+        }
+
+        std::cout << "\nFront: " << myQueue.Front() << "\n";
+        std::cout << "Back: " << myQueue.Back() << "\n\n";
+
+        std::cout << "Popping...\n";
+        int size = myQueue.Count();
+        for (int i = 0; i < size; ++i) {
+            std::cout << myQueue.Pop() << "\n";
+        }
+    } while (true);
     return 0;
 }
