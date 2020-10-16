6a90488f11c51305340bbe51a3c9b0312ec26b1c
khasanovasad <asadbek1khasanov@gmail.com>
2020-10-16 15:59:45 +0500

    testing out Stack.h

--- main.cpp
+++ main.cpp
@@ -3,13 +3,27 @@
 
 #include "SLinkedList.h"
 #include "DLinkedList.h"
+#include "Stack.h"
+
+using namespace DSCpp;
 
 int main(int args, char *argv[]) {
-    DLinkedList<std::string> d_list1 = { "zero", "one", "two", "three", "four" };
-    DLinkedList<std::string> d_list2 = d_list1;
-    for (int i = 0; i < d_list2.Count(); ++i) {
-        std::cout << d_list2.ElementAt(i) << " ";
-    }
-    std::cout << std::endl;
+    do {
+        Stack<std::string> myStack; 
+
+        std::cout << "Pushing...\n";
+        for (int i = 0; i < 10; ++i) {
+            std::cout << i << "\n";
+            myStack.Push(std::to_string(i)); 
+        }
+
+        std::cout << "\nTop: " << myStack.Top() << "\n\n";
+
+        std::cout << "Popping...\n";
+        int size = myStack.Count();
+        for (int i = 0; i < size; ++i) {
+            std::cout << myStack.Pop() << "\n";
+        }
+    } while (false);
     return 0;
 }
