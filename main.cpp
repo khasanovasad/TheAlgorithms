7fdcd16b9a66f2daf1dc5e24f2f242d237d79bdf
khasanovasad <asadbek1khasanov@gmail.com>
2020-10-19 19:04:39 +0500

    testing out Vector.h

--- main.cpp
+++ main.cpp
@@ -1,74 +1,22 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "SLinkedList.h"
 #include "DLinkedList.h"
 #include "Stack.h"
 #include "Queue.h"
 #include "Deque.h"
+#include "Vector.h"
 
 using namespace DSCpp;
 
 int main(int args, char *argv[]) {
-    /*
     do {
-        Stack<std::string> myStack; 
-
-        std::cout << "Pushing...\n";
-        for (int i = 0; i < 10; ++i) {
-            std::cout << i << "\n";
-            myStack.Push(std::to_string(i)); 
-        }
-
-        std::cout << "\nTop: " << myStack.Top() << "\n\n";
-
-        std::cout << "Popping...\n";
-        int size = myStack.Count();
-        for (int i = 0; i < size; ++i) {
-            std::cout << myStack.Pop() << "\n";
-        }
+        Vector<std::string> vect(20, "sex");
+        // vect.Clear();
+        vect.Erase(0, vect.Size());
+        std::cout << vect.Front() << std::endl;
     } while (false);
-    */
-    /*
-    do {
-        Queue<int> myQueue;
-
-        std::cout << "Pushing...\n";
-        for (int i = 0; i < 10; ++i) {
-            std::cout << i << "\n";
-            myQueue.Push(i);
-        }
-
-        std::cout << "\nFront: " << myQueue.Front() << "\n";
-        std::cout << "Back: " << myQueue.Back() << "\n\n";
-
-        std::cout << "Popping...\n";
-        int size = myQueue.Count();
-        for (int i = 0; i < size; ++i) {
-            std::cout << myQueue.Pop() << "\n";
-        }
-    } while (true);
-    */
-    /*
-        do {
-        Deque<int> myDeque;
-
-        std::cout << "PushFront...\n";
-        for (int i = 0; i < 10; ++i) {
-            std::cout << i << "\n";
-            myDeque.PushFront(i);
-        }
-
-        std::cout << "\nFront: " << myDeque.Front() << "\n";
-        std::cout << "Back: " << myDeque.Back() << "\n\n";
-
-        std::cout << "PopBack...\n";
-        int size = myDeque.Count();
-        for (int i = 0; i < size; ++i) {
-            std::cout << myDeque.PopBack() << "\n";
-        }
-    } while (false);
-    */
-    
     return 0;
 }
