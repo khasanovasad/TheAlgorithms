c4aaa6f258db34b23c1f95ae6c70847d7563a881
Asadbek Khasanov <asadbek1khasanov@gmail.com>
2021-01-02 00:40:05 +0500

    port to modern C++

--- max-subarray/brute_force.cpp
+++ max-subarray/brute_force.cpp
@@ -1,20 +1,19 @@
-#include <cstdio>
+#include <iostream>
+#include <vector>
 
-#define N 16
-
-int max_subarray(int *arr, int size);
+int max_subarray(std::vector<int>& arr);
 
 int main() {
-    int arr[N] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
-    std::printf("%d\n", max_subarray(arr, N));
+    std::vector<int> arr = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+    std::cout << max_subarray(arr) << std::endl;
     return 0;
 }
 
-int max_subarray(int *arr, int size) {
+int max_subarray(std::vector<int>& arr) {
     int best_sum = 0;
     int curr_sum = 0;
-    for (int i = 0; i < size; ++i) {
-        for (int j = i; j < size; ++j) {
+    for (int i = 0; i < arr.size(); ++i) {
+        for (int j = i; j < arr.size(); ++j) {
             curr_sum += arr[j];
             if (best_sum < curr_sum) {
                 best_sum = curr_sum;
--- max-subarray/divide_and_conquer.cpp
+++ max-subarray/divide_and_conquer.cpp
@@ -1,28 +1,25 @@
-#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 #include <climits>
 
 #define N 16
 
-int max2(int a, int b);
-int max3(int a, int b, int c);
-int find_max_crossing_subarray(int arr[], int low, int mid, int high);
-int find_max_subarray(int arr[], int low, int high);
+int find_max_crossing_subarray(std::vector<int>& arr, int low, int mid, int high);
+int find_max_subarray(std::vector<int>& arr, int low, int high);
 
 int main() {
-    int arr[N] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
-    std::printf("%d\n", find_max_subarray(arr, 0, N - 1));
+    std::vector<int> arr = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+    std::cout << find_max_subarray(arr, 0, arr.size() - 1) << std::endl;
     return 0;
 }
 
-int max2(int a, int b) {
-    return a > b ? a : b;
-}
-
 int max3(int a, int b, int c) {
-    return max2(a, b) > c ? max2(a, b) : c;
+    int a_or_b = std::max(a, b);
+    return a_or_b > c ? a_or_b : c;
 }
 
-int find_max_crossing_subarray(int arr[], int low, int mid, int high) {
+int find_max_crossing_subarray(std::vector<int>& arr, int low, int mid, int high) {
     int left_sum = INT_MIN, right_sum = INT_MIN;
     int sum;
 
@@ -42,7 +39,7 @@ int find_max_crossing_subarray(int arr[], int low, int mid, int high) {
     return max3(left_sum, right_sum, left_sum + right_sum);
 }
 
-int find_max_subarray(int arr[], int low, int high) {
+int find_max_subarray(std::vector<int>& arr, int low, int high) {
     if (low == high)
         return arr[low];
     else {
