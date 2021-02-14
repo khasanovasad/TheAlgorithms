a725551c93c859463c6b85cdc1379df46f377d76
Asadbek Khasanov <asadbek1khasanov@gmail.com>
2021-02-14 22:19:20 +0500

    added Queue.hpp

---
+++ DataStructures/Include/Queue.hpp
@@ -0,0 +1,57 @@
+#ifndef DS_QUEUE_H
+#define DS_QUEUE_H
+
+#include <stdexcept>
+
+#include "List.hpp"
+
+namespace DS
+{
+    template <typename T>
+    class Queue
+    {
+    public:
+        Queue() { }
+
+        ~Queue() { }
+
+        bool IsEmpty()
+        {
+            return _data.IsEmpty();
+        }
+
+        std::size_t Count()
+        {
+            return _data.Count();
+        }
+
+        T& Front()
+        {
+            return _data.Front();
+        }
+
+        T& Back()
+        {
+            return _data.Back();
+        }
+
+        void Push(const T& toBePushed)
+        {
+            _data.AddBack(toBePushed);
+        }
+
+        void Pop()
+        {
+            if (IsEmpty())
+            {
+                throw std::runtime_error("QueueUnderflow!");
+            }
+            _data.RemoveFront();
+        }
+
+    private:
+        List<T> _data;
+    };
+}    
+
+#endif // DS_QUEUE_H
--- DataStructures/Main.cpp
+++ DataStructures/Main.cpp
@@ -3,21 +3,21 @@
 #include "Vector.hpp"
 #include "List.hpp"
 #include "Stack.hpp"
+#include "Queue.hpp"
 
 int main(int args, char *argv[])
 {
-    DS::Stack<std::string> stackk;
+    DS::Queue<std::string> que;
 
     for (int i = 0; i < 10; ++i)
     {
-        stackk.Push(std::to_string(i) + " FAAK ");
+        que.Push(std::to_string(i) + " FAKK ");
     }
 
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < 15; ++i)
     {
-        std::cout << stackk.Top() << std::endl;
-        stackk.Pop();
+        std::cout << que.Front() << std::endl;
+        que.Pop();
     }
-
     return 0;
 }
